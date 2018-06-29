6a5f3d8d61b7e9c91dfdc1bab137c7027ced409d
arsalon <arsalon47@gmail.com>
2018-06-29 13:38:57 -0400

    reducers combined

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux'
+import cat from './cat';
+import dog from './dog';
+
+const rootReducer = combineReducers({
+  cat,
+  dog
+})
+
+export default rootReducer;
